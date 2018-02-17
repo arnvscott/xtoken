7e1b9e62d6e076afa9a16fba657161e3c2a9472f
Arnold Scott <arnvscott@gmail.com>
2018-02-17 15:47:53 +1000

    Recreated XToken from scratch to test/develop incrementally TDD

--- contracts/XToken.sol
+++ contracts/XToken.sol
@@ -1,9 +1,5 @@
 pragma solidity ^0.4.18;
 
-import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
-
-contract XToken is StandardToken {
-  string public name = "X-TOKEN";
-  string public symbol = "XTKN";
-  uint8  public decimals = 18;
+contract XToken {
+  string public name = "X-Token";
 }
