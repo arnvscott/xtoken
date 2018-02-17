589eb7cc8bf05693ebff23019bb112372de70906
Arnold Scott <arnvscott@gmail.com>
2018-02-17 17:54:12 +1000

    Reformat layout

--- test/test_xToken.js
+++ test/test_xToken.js
@@ -2,10 +2,8 @@ const XToken = artifacts.require("XToken");
 
 contract("XToken", function(accounts) {
   it("should have the name X-Token", function() {
-    return XToken.deployed()
-      .then(function(instance) {
-        return instance.name()
-          .then(function(name) {
+    return XToken.deployed().then(function(instance) {
+        return instance.name().then(function(name) {
             assert.equal(name, "X-Token", "The name of XToken is invalid")
           })
       })
