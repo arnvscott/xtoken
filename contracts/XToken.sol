325d5d775fca8347be09eabbba9179f3f79e8b91
Arnold Scott <arnvscott@gmail.com>
2018-03-06 13:11:50 +1000

    Add in the contract constructor

--- contracts/XToken.sol
+++ contracts/XToken.sol
@@ -1,8 +1,26 @@
 pragma solidity ^0.4.18;
 
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
-contract XToken {
-  string public name = "X-Token";
-  string public symbol = "XTKN";
-  uint8  public decimals = 18;
+contract XToken is StandardToken {
+
+  string public constant name = "X-Token";
+  string public constant symbol = "XTKN";
+  uint8  public constant decimals = 18;
+
+  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  function XToken() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  }
+
+function () public payable {
+
+}
+
 }
